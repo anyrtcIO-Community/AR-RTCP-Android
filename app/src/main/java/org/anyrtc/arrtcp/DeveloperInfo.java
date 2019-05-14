df033e34230118f728076e26ed291afc8c61eddb
liuxiaozhong <liuxiaozhong@dync.cc>
2019-05-14 11:07:35 +0800

    commit

--- app/src/main/java/org/anyrtc/arrtcp/DeveloperInfo.java
+++ app/src/main/java/org/anyrtc/arrtcp/DeveloperInfo.java
@@ -9,21 +9,21 @@ public class DeveloperInfo {
      * anyRTC 平台的开发者ID
      */
 
-    public final static String DEVELOPERID = "95878050";
+    public final static String DEVELOPERID = "";
 
     /**
      * anyRTC 平台的appid
      */
-    public final static String APPID = "anyrtcy0UkK1PZydLX";
+    public final static String APPID = "";
 
     /**
      * anyRTC 平台的appkey
      */
-    public final static String APPKEY = "q50MGCmTkDn/wXBWNY5YjnB54KOzwwV8RoQ02AV6R9Y";
+    public final static String APPKEY = "";
 
     /**
      * anyRTC 平台的apptoken
      */
-    public final static String APPTOKEN = "fabf75cfe0090ef495bf628cd7b9270b";
+    public final static String APPTOKEN = "";
 
 }
