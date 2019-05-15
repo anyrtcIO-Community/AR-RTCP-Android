2a499c90f92dbe2fa3c827d4be1cb6b1375a6f70
liuxiaozhong <liuxiaozhong@dync.cc>
2019-05-16 05:02:56 +0800

    修改

--- app/src/main/java/org/anyrtc/arrtcp/ARApplication.java
+++ app/src/main/java/org/anyrtc/arrtcp/ARApplication.java
@@ -14,7 +14,8 @@ public class ARApplication extends Application {
     public void onCreate() {
         super.onCreate();
         //初始化RTCP引擎,配置开发者信息 可去anyrtc.io官网注册获取
-        ARRtcpEngine.Inst().initEngineWithARInfo(getApplicationContext(),  DeveloperInfo.DEVELOPERID,DeveloperInfo.APPID, DeveloperInfo.APPKEY, DeveloperInfo.APPTOKEN);
+        ARRtcpEngine.Inst().initEngine(getApplicationContext(),  DeveloperInfo.APPID,  DeveloperInfo.APPTOKEN);
+
     }
 
 
--- app/src/main/java/org/anyrtc/arrtcp/DeveloperInfo.java
+++ app/src/main/java/org/anyrtc/arrtcp/DeveloperInfo.java
@@ -9,21 +9,21 @@ public class DeveloperInfo {
      * anyRTC 平台的开发者ID
      */
 
-    public final static String DEVELOPERID = "";
+    public final static String DEVELOPERID = "95878050";
 
     /**
      * anyRTC 平台的appid
      */
-    public final static String APPID = "";
+    public final static String APPID = "anyrtcy0UkK1PZydLX";
 
     /**
      * anyRTC 平台的appkey
      */
-    public final static String APPKEY = "";
+    public final static String APPKEY = "q50MGCmTkDn/wXBWNY5YjnB54KOzwwV8RoQ02AV6R9Y";
 
     /**
      * anyRTC 平台的apptoken
      */
-    public final static String APPTOKEN = "";
+    public final static String APPTOKEN = "fabf75cfe0090ef495bf628cd7b9270b";
 
 }
